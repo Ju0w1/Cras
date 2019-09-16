bfbc3e25e62eeb33ea3b429d2a0b24433aad2f36
Enzo Nicolas Stagno Piriz <nicolasstagno@cras-dev.com>
2019-09-16 19:16:22 -0300

    Trabajando en el login

--- lib/Pantallas/Logueo.dart
+++ lib/Pantallas/Logueo.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 int _darkBlue = 0xFF022859;
 int _midBlue = 0xFF2E78A6;
 int _lightBlue = 0xFF6AAED9;
-
+const _url = "https://www.cras-dev.com/Interfaz/interfaz.php?auth=4kebq1J2MD";
 
 class Logueo extends StatefulWidget{
   @override
@@ -43,6 +43,55 @@ class _Logueo extends State<Logueo>{
                             fillColor: Colors.white,
                             labelStyle: TextStyle(color: Colors.grey),
                             prefixIcon: Icon(Icons.email,color: Color(_darkBlue),),
+                            labelText: "Email",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8))
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+                        TextField(
+                          controller: pwd,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            labelStyle: TextStyle(color: Colors.grey),
+                            prefixIcon: Icon(Icons.vpn_key,color: Color(_darkBlue),),
+                            labelText: "Contraseña",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8))
+                            ),
+
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          alignment: Alignment.topRight,
+                          child: InkWell(
+                            onTap: (){},
+                            child: Text(
+                              'No recuerda su contraseña?',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50,),
+                        Container(
+                          height: 40,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            shadowColor: Colors.blueAccent,
+                            color: Color(_darkBlue),
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () async{
+                                  var response = await http.get();
+                              },
+                            ),
                           ),
                         )
                       ],
