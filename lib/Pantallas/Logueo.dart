9ae0950994b26bcb9fbeb76dda4ed8213b10dbe7
Enzo Nicolas Stagno Piriz <nicolasstagno@cras-dev.com>
2019-09-16 16:21:36 -0300

    Se modifico el main y se agrego el dart de logueo

---
+++ lib/Pantallas/Logueo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Logueo extends StatefulWidget{
+  @override
+  _Logueo createState() => _Logueo();
+}
+
+class _Logueo extends State<Logueo>{
+  @override
+  Widget build(BuildContext context){
+    return null;
+  }
+}
